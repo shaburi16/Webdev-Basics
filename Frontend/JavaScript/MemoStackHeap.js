d3d6a67aeb3d036bddc97c998a1890b0344a655f
shaburi16 <shaburi16@gmail.com>
2026-05-03 22:24:47 +0530

    MemoStackHeap

---
+++ Frontend/JavaScript/MemoStackHeap.js
@@ -0,0 +1,7 @@
+// Stack memory is a fast, automatically managed, and LIFO region for local variables and function calls, limited in size. (PRIMITIVE)
+// Heap memory is a larger, slower, dynamically managed region used for long-lived objects and global data, requiring explicit allocation/deallocation(NON PRIMITIVE)
+
+let myName = "Anu"
+let anotherName = "Rahul"
+
+console.log(anotherName);
